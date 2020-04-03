914eff33d8ba9e10e660536b26b06837edc7c8cf
adrixop95 <adrixop95@me.com>
2020-04-03 11:07:21 +0200

    add while-true in entrypoint.sh with sleep for continuous data upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,7 @@ do
             INPUT)    INPUT=${VALUE} ;;     
             TIMECOLUMN)    INPUT=${VALUE} ;;
             SERVER)    INPUT=${VALUE} ;;
+            TIME_SECCONDS)    INPUT=${VALUE} ;;
             *)   
     esac    
 
@@ -22,9 +23,14 @@ echo "DBNAME = $DBNAME"
 echo "INPUT = $INPUT" 
 echo "TIMECOLUMN = $TIMECOLUMN" 
 echo "SERVER = $SERVER"
+echo "TIME_SECCONDS = $TIME_SECCONDS"
 echo "================="
 
 
 /app/wait-for-it.sh influxdb:8086 -t 15 -- echo "Database is up!"
 
-python /app/csv-to-influxdb.py --server $SERVER --dbname $DBNAME --input $INPUT --timecolumn $TIMECOLUMN -d ";" --fieldcolumns "wilg,temp >2m,ciś,opad,nasłon,temp < 2m,pokrywa śnieżna,temp <2m min,temp <2m max,Vwmax,Vwsr,Vwmin,kierunek wsr,kierunek wmax,kierunek wmin" --timeformat "%Y-%m-%d %H:%M:%S"
+while true
+do
+    python /app/csv-to-influxdb.py --server $SERVER --dbname $DBNAME --input $INPUT --timecolumn $TIMECOLUMN -d ";" --fieldcolumns "wilg,temp >2m,ciś,opad,nasłon,temp < 2m,pokrywa śnieżna,temp <2m min,temp <2m max,Vwmax,Vwsr,Vwmin,kierunek wsr,kierunek wmax,kierunek wmin" --timeformat "%Y-%m-%d %H:%M:%S"
+    sleep $TIME_SECCONDS
+done
